d1638b984da9ded1fca62cd68daf295d508965de
shiichi <s.shiichi311041@gmail.com>
2016-06-19 13:32:26 +0900

    modify signup responce

--- app/Http/Controllers/User/Auth/AuthController.php
+++ app/Http/Controllers/User/Auth/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
 
         $token = JWTAuth::fromUser($user);
 
-        return $token;
+        return ['token' => $token];
     }
 
     public function signin(SigninRequest $request)
